0f3421c285ca431a444c853dd383c9588390aa9e
aayalac <aayalac@misena.edu.co>
2022-09-05 11:05:41 -0500

    Se crean las entidades Bus y estacion

---
+++ AplicacionMVC/Models/Bus.cs
@@ -0,0 +1,31 @@
+﻿namespace AplicacionMVC.Models
+{
+    public class Bus
+    {
+        public string NombreRuta { get;  private set; }
+
+        public string Placa { get; private set; }
+
+        public string Marca { get; private set; }
+
+        public string Modelo { get; private set; }
+
+        public Guid EstacionId { get; private set; } // foreing key
+
+        public Estacion Estacion { get; private set; }
+
+        private Bus(Guid id,string nombreRuta, string placa, string marca, string modelo, Guid estacionId)
+        {
+            NombreRuta = nombreRuta;
+            Placa = placa;
+            Marca = marca;
+            EstacionId = estacionId;
+            Modelo = modelo;
+        }
+
+        public Bus Build(Guid id, string nombreRuta, string placa, string marca, string modelo, Guid estacionId)
+        {
+            return new Bus(id,nombreRuta,placa,marca,modelo,estacionId);
+        }
+    }
+}
---
+++ AplicacionMVC/Models/Estacion.cs
@@ -0,0 +1,30 @@
+﻿namespace AplicacionMVC.Models
+{
+    public class Estacion : Entity
+    {
+        // atributos
+        public string Nombre { get; private set; }
+
+        public string Troncal { get; private set; }
+
+        public string Direccion { get; private set; }
+
+        public int NumeroVagones { get; private set; }
+
+        // lista buses
+        public List<Bus> Buses { get; private set; } 
+
+        private Estacion(Guid id, string nombre, string troncal, string direccion, int numeroVagones) : base()
+        {
+            Nombre = nombre;
+            Troncal = troncal;
+            Direccion = direccion;
+            NumeroVagones = numeroVagones;
+        }
+
+        public Estacion Build(Guid id, string nombre, string troncal, string direccion, int numeroVagones)
+        {
+            return new Estacion(id,nombre,troncal,direccion,numeroVagones);
+        }
+    }
+}
