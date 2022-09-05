ae4d7e7fbe0b53dded9074117dbe9143e876a38d
aayalac <aayalac@misena.edu.co>
2022-09-05 10:36:49 -0500

    Creacion de modelos iniciales

---
+++ AplicacionMVC/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace AplicacionMVC.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        protected Entity()
+        {
+
+        }
+
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
